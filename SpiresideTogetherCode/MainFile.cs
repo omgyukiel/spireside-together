0f7467e0aa68ef90db10317c7127e7e0fc155aea
yukiel <kennylevu1@gmail.com>
2026-04-29 00:23:28 -0700

    chore: scaffoled SpiresideTogether mod template

---
+++ SpiresideTogetherCode/MainFile.cs
@@ -0,0 +1,21 @@
+using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Modding;
+
+namespace SpiresideTogether.SpiresideTogetherCode;
+
+//You're recommended but not required to keep all your code in this package and all your assets in the SpiresideTogether folder.
+[ModInitializer(nameof(Initialize))]
+public partial class MainFile : Node
+{
+    public const string ModId = "SpiresideTogether"; //At the moment, this is used only for the Logger and harmony names.
+
+    public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } = new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
+
+    public static void Initialize()
+    {
+        Harmony harmony = new(ModId);
+
+        harmony.PatchAll();
+    }
+}
