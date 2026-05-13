62eab36d115bdac908dc73e6279ed68e7e099d21
KennyLevu <kennylevu1@gmail.com>
2026-05-13 00:38:04 -0700

    feat: apply postfix patch to set all multiplayer lobbies to public

--- SpiresideTogetherCode/Patches/SteamHostPublicLobbyPatch.cs
+++ SpiresideTogetherCode/Patches/SteamHostPublicLobbyPatch.cs
@@ -1,6 +1,37 @@
-namespace SpiresideTogether.SpiresideTogetherCode;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.Multiplayer;
+using MegaCrit.Sts2.Core.Multiplayer.Transport.Steam;
+using Steamworks;
+using System.Threading.Tasks;
 
+namespace SpiresideTogether.SpiresideTogetherCode.Patches;
+
+/*
+   Change the lobby type from friends -> public. In MegaCrit.Sts2.Core.Multiplayer.Transport.Steam,
+   the CSteamID is created and stored in the SteamHost instance only when the task result resolves
+   to EResult.k_EResultOK. We use this status to implicitly infer the availability of the lobby id
+   to modify the lobby type after StartHost 
+*/
+[HarmonyPatch(typeof(SteamHost), nameof(SteamHost.StartHost))]
 public class SteamHostPublicLobbyPatch
 {
-    
+    private static void Postfix(SteamHost __instance, ref Task<NetErrorInfo?> __result)
+    {
+        __result = MakeLobbyPublicAfterStart(__instance, __result);
+    }
+    private static async Task<NetErrorInfo?> MakeLobbyPublicAfterStart(
+        SteamHost host,
+        Task<NetErrorInfo?> startHostTask)
+    {
+        // Wait for original startHost task to complete before checking LobbyId
+        var result = await startHostTask;
+        MainFile.Logger.Info($"Steam host startup complete. Error: {result.HasValue.ToString()}");
+        // null result means successful lobby completion, so return if result has value
+        if (result.HasValue || !host.LobbyId.HasValue) return result;
+        MainFile.Logger.Info($"Steam lobby id is {host.GetRawLobbyIdentifier()}");
+        SteamMatchmaking.SetLobbyType(host.LobbyId.Value, ELobbyType.k_ELobbyTypePublic);
+        MainFile.Logger.Info("Steam lobby set to public");
+
+        return result;
+    }
 }
